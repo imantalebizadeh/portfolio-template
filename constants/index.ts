2caf9eedcf699af5fdea98a5073b4b28382c542a
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2025-01-03 19:21:17 +0330

    Add NAV_ITEMS constant for navigation links

---
+++ constants/index.ts
@@ -0,0 +1,6 @@
+export const NAV_ITEMS: { id: number; title: string; url: string }[] = [
+  { id: 1, title: "Home", url: "#" },
+  { id: 2, title: "Projects", url: "#" },
+  { id: 3, title: "About", url: "#" },
+  { id: 4, title: "Contact", url: "#" },
+];
