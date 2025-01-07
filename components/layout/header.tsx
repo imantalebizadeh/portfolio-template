3616f861d28f78c889c1960917e3b15c438f0eb3
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2025-01-07 12:33:23 +0330

    Refactor layout structure by moving Header to the layout directory

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
   const [activeNavItem, setActiveNavItem] = useState(1);
 
   return (
-    <header className="fixed left-1/2 right-1/2 top-3 flex items-center justify-center">
+    <header className="fixed left-1/2 right-1/2 top-3 z-10 flex items-center justify-center">
       <nav className="flex gap-x-1.5 rounded-full border border-white/10 bg-white/10 p-1 backdrop-blur">
         {NAV_ITEMS.map((navItem) => {
           const isActive = navItem.id === activeNavItem;
