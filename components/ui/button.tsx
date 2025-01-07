ffce27de82ff85aa0a47990280234e8b16ba248d
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2025-01-07 12:34:26 +0330

    Add Button component with variant support and styling

---
+++ components/ui/button.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+import { cva, type VariantProps } from "class-variance-authority";
+
+const buttonVariants = cva(
+  "inline-flex items-center px-6 gap-2 h-12 whitespace-nowrap rounded-lg text-sm font-semibold transition-colors disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  {
+    variants: {
+      variant: {
+        default: "bg-white text-gray-900 hover:bg-gray-100 hover:text-gray-900",
+        outline: "border border-white/15",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  },
+);
+
+interface ButtonProps
+  extends React.ComponentProps<"button">,
+    VariantProps<typeof buttonVariants> {}
+
+function Button({ className, variant, ...props }: ButtonProps) {
+  return (
+    <button
+      type="button"
+      className={cn(buttonVariants({ variant }), className)}
+      {...props}
+    >
+      {props.children}
+    </button>
+  );
+}
+
+Button.displayName = "Button";
+
+export { Button, buttonVariants };
