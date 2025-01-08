6b8bce8ace82bd997553a49d7e104b161705fa52
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2025-01-08 22:34:05 +0330

    Update Button component to center content with justify-center class for improved alignment

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils";
 import { cva, type VariantProps } from "class-variance-authority";
 
 const buttonVariants = cva(
-  "inline-flex items-center px-6 gap-2 h-12 whitespace-nowrap rounded-lg text-sm font-semibold transition-colors disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "inline-flex items-center justify-center px-6 gap-2 h-12 whitespace-nowrap rounded-lg text-sm font-semibold transition-colors disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
   {
     variants: {
       variant: {
