ea3f6ff5a3ce76279f91084b2fe18492e872b0f6
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2025-01-13 20:48:55 +0330

    Refactor ProjectsFeaturesTap component to implement a continuous scrolling animation for project features

--- components/layout/projects-features-tap.tsx
+++ components/layout/projects-features-tap.tsx
@@ -1,3 +1,5 @@
+import { Fragment } from "react";
+
 import { PROJECTS_FEATURES } from "@/constants";
 import { StarIcon } from "../icons";
 
@@ -5,15 +7,22 @@ export default function ProjectsFeaturesTap() {
   return (
     <div className="overflow-x-clip py-16 xl:py-24">
       <div className="-mx-1 -rotate-3 bg-gradient-to-r from-emerald-300 to-sky-400">
-        <div className="flex justify-center [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
-          <div className="flex flex-none gap-x-4 py-3">
-            {PROJECTS_FEATURES.map((feature) => (
-              <div key={feature} className="inline-flex items-center gap-x-4">
-                <span className="text-sm font-extrabold uppercase text-gray-900">
-                  {feature}
-                </span>
-                <StarIcon className="size-6 -rotate-12 text-gray-900" />
-              </div>
+        <div className="mask-image flex">
+          <div className="flex flex-none animate-move-left gap-x-4 py-3 pr-4 [animation-duration:30s]">
+            {Array.from({ length: 2 }).map((_, index) => (
+              <Fragment key={index}>
+                {PROJECTS_FEATURES.map((feature) => (
+                  <div
+                    key={feature}
+                    className="inline-flex items-center gap-x-4"
+                  >
+                    <span className="text-sm font-extrabold uppercase text-gray-900">
+                      {feature}
+                    </span>
+                    <StarIcon className="size-6 -rotate-12 text-gray-900" />
+                  </div>
+                ))}
+              </Fragment>
             ))}
           </div>
         </div>
