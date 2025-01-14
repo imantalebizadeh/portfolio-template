a11cafe6a5eb434dc61ce35d085a7324bc7e39a1
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2025-01-14 12:28:43 +0330

    Refactor Card components to accept additional props for improved flexibility and reusability

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,12 +1,13 @@
 import { cn } from "@/lib/utils";
 
-function Card({ children, className }: React.ComponentProps<"div">) {
+function Card({ children, className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       className={cn(
         "relative z-0 overflow-hidden rounded-3xl bg-gray-800 after:pointer-events-none after:absolute after:inset-0 after:z-10 after:rounded-3xl after:border-2 after:border-white/20 after:content-['']",
         className,
       )}
+      {...props}
     >
       <div className="absolute inset-0 -z-10 bg-[url('/images/grain.jpg')] opacity-5" />
 
@@ -15,27 +16,46 @@ function Card({ children, className }: React.ComponentProps<"div">) {
   );
 }
 
-function CardHeader({ children, className }: React.ComponentProps<"div">) {
-  return (
-    <div className={cn("flex flex-col p-6 md:px-10 md:py-8", className)}>
-      {children}
-    </div>
-  );
-}
-
-function CardTitle({ children, className }: React.ComponentProps<"div">) {
+function CardHeader({
+  children,
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
   return (
     <div
-      className={cn("flex items-center gap-x-2 font-serif text-3xl", className)}
+      className={cn("flex flex-col p-6 md:px-10 md:py-8", className)}
+      {...props}
     >
       {children}
     </div>
   );
 }
 
-function CardDescription({ children, className }: React.ComponentProps<"p">) {
+function CardTitle({
+  children,
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
   return (
-    <p className={cn("mt-2 text-sm text-white/60 xl:text-base", className)}>
+    <div
+      className={cn("flex items-center gap-x-2 font-serif text-3xl", className)}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
+
+function CardDescription({
+  children,
+  className,
+  ...props
+}: React.ComponentProps<"p">) {
+  return (
+    <p
+      className={cn("mt-2 text-sm text-white/60 xl:text-base", className)}
+      {...props}
+    >
       {children}
     </p>
   );
