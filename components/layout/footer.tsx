b51c51840b55d2eb07cc65cf61662c540f473022
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2025-01-14 12:25:04 +0330

    Refactor Footer component to improve accessibility by adding pointer-events-none class to background div

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -3,8 +3,8 @@ import { ArrowUpRightIcon } from "../icons";
 
 export default function Footer() {
   return (
-    <footer className="relative -z-10 overflow-x-clip">
-      <div className="absolute bottom-0 left-1/2 -z-10 h-[400px] w-[1600px] -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)]" />
+    <footer className="relative overflow-x-clip">
+      <div className="pointer-events-none absolute bottom-0 left-1/2 -z-10 h-[400px] w-[1600px] -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)]" />
 
       <div className="container">
         <div className="flex flex-col items-center gap-8 border-t border-white/15 py-6 text-sm md:flex-row md:justify-between">
